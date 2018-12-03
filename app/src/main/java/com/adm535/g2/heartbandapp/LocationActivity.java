6df745c12d93791a4bb699614996f1d4b0d392f4
victordmontero <victordanielmontero@gmail.com>
2018-12-03 14:43:03 -0400

    Agregando actualizacion de direccion y coordenadas

--- app/src/main/java/com/adm535/g2/heartbandapp/LocationActivity.java
+++ app/src/main/java/com/adm535/g2/heartbandapp/LocationActivity.java
@@ -1,8 +1,11 @@
 package com.adm535.g2.heartbandapp;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.adm535.g2.heartbandapp.models.HeartBandData;
@@ -16,7 +19,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -31,22 +36,35 @@ public class LocationActivity extends AppCompatActivity {
         final TextView longitudeText = findViewById(R.id.longitud);
         final TextView address = findViewById(R.id.address);
 
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 map = googleMap;
-                ArrayList<HeartBandData> results = QueryUtils.getHeartBandData();
-                LatLng pos = results.get(0).toLatLng();
+                LatLng pos = QueryUtils.getHeartBandData().toLatLng();
 
-                map.moveCamera(CameraUpdateFactory.newLatLng(pos));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 15));
                 map.addMarker(new MarkerOptions().position(pos));
 
-                latitudeText.setText(Double.toString(pos.latitude));
-                longitudeText.setText(Double.toString(pos.longitude));
+                latitudeText.setText(QueryUtils.ConvertToDMS(pos.latitude));
+                longitudeText.setText(QueryUtils.ConvertToDMS(pos.longitude));
 
+                Geocoder geocoder = new Geocoder(LocationActivity.this);
+
+                try {
+                    List<Address> list = geocoder.getFromLocation(pos.latitude, pos.longitude, 3);
+                    Address addr = list.get(0);
+                    address.setText(String.format("%s %s,%s,%s", addr.getThoroughfare(),
+                            addr.getSubThoroughfare() == null ? "" : addr.getSubThoroughfare(),
+                            addr.getSubLocality(), addr.getAdminArea()));
+                } catch (IOException iox) {
+                    address.setText("No Address");
+                } catch (Exception ex) {
+                    Log.d("GEOCODER_ERROR", "onMapReady: Error: " + ex.getMessage());
+                    address.setText("No Address");
+                }
             }
         });
     }
--- app/src/main/java/com/adm535/g2/heartbandapp/QueryUtils.java
+++ app/src/main/java/com/adm535/g2/heartbandapp/QueryUtils.java
@@ -6,29 +6,25 @@ import java.util.ArrayList;
 
 public class QueryUtils {
 
-    public static ArrayList<HeartBandData> getHeartBandData() {
-        ArrayList<HeartBandData> data = new ArrayList<>();
+    public static HeartBandData getHeartBandData() {
 
-        data.add(new HeartBandData(
+        return new HeartBandData(
                 new float[]{2.0f, 3.0f},
                 new float[]{1.0f, 2.0f},
-                18.6000f,
-                -63.5432f
-        ));
-        data.add(new HeartBandData(
-                new float[]{6.0f, 5.0f},
-                new float[]{3.0f, 4.0f},
-                18.6000f,
-                -63.5432f
-        ));
-        data.add(new HeartBandData(
-                new float[]{8.0f, 6.0f},
-                new float[]{10.0f, 8.0f},
-                18.6000f,
-                -63.5432f
-        ));
+                18.473956f,
+                -69.913072f);
+    }
 
-        return data;
+    public static String ConvertToDMS(double value, String format) {
+        value = Math.abs(value);
+        int degree = (int) value;
+        int minutes = (int) ((value - degree) * 60);
+        int seconds = (int) (((value - degree) * 60) - minutes) * 60;
+        return String.format(format, degree, minutes, seconds);
+    }
+
+    public static String ConvertToDMS(double value) {
+        return ConvertToDMS(value, "%d°%d\'%d\"");
     }
 
 }
